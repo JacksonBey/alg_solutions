89bc1688a1289b9ee28beaaaa2683f08e02f3788
JacksonBey <jacksongrowson@gmail.com>
2021-02-15 15:08:50 -0800

    added description and inputs

--- leetcode/fizz-buzz.js
+++ leetcode/fizz-buzz.js
@@ -1,3 +1,13 @@
+// Write a program that outputs the string representation of numbers from 1 to n.
+
+// But for multiples of three it should output “Fizz” instead of the number
+//  and for the multiples of five output “Buzz”. 
+//  For numbers which are multiples of both three and five output “FizzBuzz”.
+
+let n = 15
+
+
+
 var fizzBuzz = function(n) {
     let array = []
     for(let i=1;i<=n;i++){
