793d051af84f4569eed6a16461884e1a74b5346d
JacksonBey <jacksongrowson@gmail.com>
2021-02-22 17:15:46 -0800

    added step by step for Reverse Integer

--- leetcode/Reverse_Integer.js
+++ leetcode/Reverse_Integer.js
@@ -6,3 +6,13 @@
 //  * @param {number} x
 //  * @return {number}
 //  */
+
+// 1. check if negative number
+// if negative, splice after index 0 and set isNegative variable to true
+// 2. check if 0
+// 3. split into array, reverse
+// 4. iterate over array starting from 0th index to first integer > 0,
+// splice all leading zeros out
+// 5. join array
+// 6. if isNegative is true, multiply by -1 
+//for (i = 0; i < 5; i++) 
