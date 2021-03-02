50528dad7c86aea73c6b8e71cb197da05a78ff82
JacksonBey <jacksongrowson@gmail.com>
2021-03-01 18:05:18 -0800

    added input output

--- lonelyInteger.js
+++ lonelyInteger.js
@@ -2,3 +2,6 @@
 // There are N integers in an array A. 
 // All but one integer occurs in pairs. 
 // Your task is to find out the number that occurs only once.
+
+// input: array of integers
+// output: integer
