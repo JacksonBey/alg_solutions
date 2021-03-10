f0c5b11a3f3b9940c3ff71b29983e7b083d876d7
JacksonBey <jacksongrowson@gmail.com>
2021-03-09 16:57:37 -0800

    added function in I/O

--- leetcode/powerOfThree.js
+++ leetcode/powerOfThree.js
@@ -2,3 +2,10 @@
 // Otherwise, return false.
 // An integer n is a power of three, 
 // if there exists an integer x such that n == 3x.
+
+// input integer n
+// return true/false
+
+const is_power_of_three = (n) => {
+
+}
