79fbed671d01e8fc5380c59ec1d04f038b44736b
JacksonBey <jacksongrowson@gmail.com>
2021-03-11 14:21:04 -0800

    added string method to po3

--- leetcode/powerOfThree.js
+++ leetcode/powerOfThree.js
@@ -22,11 +22,17 @@ const is_power_of_three = (n) => {
 // console.log('should be: true', is_power_of_three(n3))
 
 const is_power_of_three2=(exp,num)=>{
-  console.log(exp**parseInt((Math.log(num)/Math.log(exp))))
-  console.log(num)
   return parseInt(exp**parseInt((Math.log(num)/Math.log(exp))))===num
 }
 
-console.log('should be: true', is_power_of_three2(3,n1))
-console.log('should be: false', is_power_of_three2(3,n2))
-console.log('should be: true', is_power_of_three2(3,n3))
+// console.log('should be: true', is_power_of_three2(3,n1))
+// console.log('should be: false', is_power_of_three2(3,n2))
+// console.log('should be: true', is_power_of_three2(3,n3))
+
+const is_power_of_three3=(n)=>{
+  return parseFloat((Math.log(n)/Math.log(3)).toString().substr(0,4))%1 ===0
+}
+
+console.log('should be: true', is_power_of_three3(n1))
+console.log('should be: false', is_power_of_three3(n2))
+console.log('should be: true', is_power_of_three3(n3))
