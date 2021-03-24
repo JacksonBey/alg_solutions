c897ac454ca463d878ca748f137f6bde7e3754a4
JacksonBey <jacksongrowson@gmail.com>
2021-03-24 14:41:54 -0700

    added linked list definition

--- leetcode/PalindromeLinkedList.js
+++ leetcode/PalindromeLinkedList.js
@@ -1,1 +1,9 @@
 // Given the head of a singly linked list, return true if it is a palindrome.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
