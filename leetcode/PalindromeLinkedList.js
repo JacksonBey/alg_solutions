f2e929c4bb6ee9e9c5168107df608c389d26347d
JacksonBey <jacksongrowson@gmail.com>
2021-03-24 14:45:25 -0700

    added palendrome linked list solution

--- leetcode/PalindromeLinkedList.js
+++ leetcode/PalindromeLinkedList.js
@@ -14,5 +14,21 @@
 //  */
 
 var isPalindrome = function(head) {
-
+  let arr = []
+  while (head){
+      arr.push(head.val)
+      head=head.next
+  }
+  let i=0
+  if(arr.length===1 || (arr.length===2 && arr[0]===arr[1]) || (arr.length===3 && arr[0]===arr[2])){
+      return true
+  }
+  while (i<=arr.length/2){
+      if (arr[i] !== arr[arr.length-(i+1)]){
+          console.log('i: ', arr[i], '.length-i', arr[arr.length-(i+1)])
+          return false
+      }
+      i++
+  }
+  return true
 }
