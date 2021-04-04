c1380b21abf927a76968ee976dc5da931e9ee85f
JacksonBey <jacksongrowson@gmail.com>
2021-04-04 13:26:40 -0700

    added longest substring solution

--- leetcode/LongestSubString.js
+++ leetcode/LongestSubString.js
@@ -6,5 +6,26 @@
 //  */
 
 var lengthOfLongestSubstring = function(s) {
-
+  if (s === '') {return 0
+  } else if(s.length === 1) {return 1}
+  let arr = s.split('')
+  let max = 0
+  arr.forEach((l,index) => {
+      let narr = []
+    for (let i =index; i <= arr.length; i++) {
+              if (narr.length > max){
+          max = narr.length
+        }
+      if (narr.includes(arr[i])) {
+        if (narr.length > max){
+          max = narr.length
+        }
+        break
+      } else {
+        // console.log(arr[i])
+        narr.push(arr[i])
+      }
+    }
+  })
+  return max
 };
